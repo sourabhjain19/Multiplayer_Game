41b82e186e02a7a95feee226de9513b2cc2e5d70
sourabhjain19 <sourabhjain1991999@gmail.com>
2020-04-07 15:28:11 +0530

    Completed OEE

--- Agar.IO_Client3.py
+++ Agar.IO_Client3.py
@@ -1,153 +1,51 @@
 import pygame
-import time
 import random
+import socket
+import pickle
 pygame.init()
 
-#Setting Background Color for the window
 background_colour = (255,255,255)
-
-#Setting Width and Height of the Frame or Window
 (width, height) = (800, 800)
-
-#Creating window for the game by setting Background Color,Width and Height
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Agar.IO')
 screen.fill(background_colour)
 
-clock = pygame.time.Clock()
-pygame.time.set_timer(pygame.USEREVENT, 1000)
+class Network():
 
-class GameController:    
-    #initial parameters
+    #Intilizing the socket and connecting to server
     def __init__(self):
-        self.randomlist=[]
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect(("127.0.0.1",5001))
 
-    def updaterandomlist(self,list1):
-        self.randomlist=list1
+    #Send randompoints and this player blob's position to other players
+    def send(self,datatobesent):
+        msg = pickle.dumps(datatobesent)
+        self.s.send(msg)
 
-    def getrandomlist(self):
-        return self.randomlist
-    #Function to generate random points and color sequence
-    def refreshpoint(self):
-        self.randomlist=[]
-        for i in range(200):
-            randomele=[random.randrange(0,255),random.randrange(0,255),random.randrange(0,255),random.randrange(0,800),random.randrange(0,800)]
-            self.randomlist.append(randomele)
+    #Recieve randompoints and other players blob's position
+    def recieve(self):
+        msg = self.s.recv(4096)
+        fullMsg=pickle.loads(msg)
+        return fullMsg
 
-    #Function to redraw the frame by creating random dots(small points) and color
-    def redraw(self):
-        screen.fill(background_colour)
-        for i in range(len(self.randomlist)):
-            color=(self.randomlist[i][0],self.randomlist[i][1],self.randomlist[i][2])
-            pygame.draw.circle(screen, color, (self.randomlist[i][3],self.randomlist[i][4]), 3)
-
-    #Function to remove dots on collision with userblob
-    def removepoints(self,x,y,r):
-        for i in self.randomlist:
-            if ((i[3] - int(x))**2 + (i[4] - int(y))**2 )< int(r)**2:
-                r+=0.5
-                self.randomlist.remove(i)
-        return r
-    
-    #refresh points if points less than 100
-    def refreshpoints(self):
-        if len(self.randomlist)<100:
-            self.refreshpoint()
-
-class Game():
-
-    def __init__(self):
-        self.counter=0
-        self.GameController=GameController()
-        self.player=Player((0,0,0))
-        self.anotherplayer=[Player((random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))),Player((random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))),Player((random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))),Player((random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))),Player((random.randrange(0,255),random.randrange(0,255),random.randrange(0,255)))]
-        self.network=Network()
-
-    def updatedata(self,fullMsg,gamerunning):
-
-        self.GameController.updaterandomlist(fullMsg[0])
-        for i in range(len(fullMsg[1])):
-            self.anotherplayer[i].setposition(fullMsg[1][i][0],fullMsg[1][i][1],fullMsg[1][i][2])
-            x1,y1,r1=self.player.getposition()
-            x2,y2,r2=fullMsg[1][i][0],fullMsg[1][i][1],fullMsg[1][i][2]
-            if ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) < ((r1 + r2) * (r1 + r2)):
-                if r1<r2:
-                    self.player.setposition(x1,y1,0)
-                    gamerunning=False
-                
-        return gamerunning
-        
-    def run(self):
-        
-        x=0
-        y=0
-        r=0
-        running = True
-        gamerunning=True
-
-        while running:
-             
-            self.network.send([self.GameController.getrandomlist(),x,y,r])
-
-            fullMsg=self.network.recieve()
-
-            if gamerunning:
-                
-                gamerunning=self.updatedata(fullMsg,gamerunning)
-
-                self.GameController.redraw()
-
-                for i in range(len(fullMsg[1])):
-                    self.anotherplayer[i].run()
-
-                self.GameController.refreshpoints()
-                
-                self.counter=fullMsg[2]
-                text = str(self.counter)
-                font = pygame.font.SysFont('Consolas', 30)
-                screen.blit(font.render(text, True, (0, 0, 0)), (10, 10))
-                
-                self.player.run()
-                self.player.key_operation()
-                x,y,r=self.player.getposition()
-                r=self.GameController.removepoints(x,y,r)
-                self.player.setposition(x,y,r)
-
-                pygame.display.update()
-                
-                if self.counter==30:
-                    gamerunning=False
-            else:
-                font = pygame.font.SysFont('Consolas', 60)
-                #self.network.s.close()
-                x1,y1,r1=self.player.getposition()
-                radius_array=[0,0,0,0,0]
-                for i in range(5):
-                    x2,y2,radius_array[i]=self.anotherplayer[i].getposition()
-                if r1>max(radius_array):
-                    text="Game Over, You WON"
-                else:
-                    text="Game Over, You LOSE"
-
-                screen.fill(background_colour)
-                screen.blit(font.render(text, True, (0, 0, 0)), (50, 400))
-                pygame.display.update()
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.network.s.close()
-                    running = False
-                    
 class Player:
     
+    #Intilizing the variables 
     def __init__(self,color):
-        self.x=400
-        self.y=400
+        self.x=200
+        self.y=200
         self.r=15
         self.color=color
     
+    #Creating the Circular Blob
     def run(self):
         pygame.draw.circle(screen, self.color,(int(self.x),int(self.y)), int(self.r))
+    
+    #Text on Blob indication this player's blob
+    def renderText(self):
+        text = "User"
+        font = pygame.font.SysFont('Consolas', int(self.r/2))
+        screen.blit(font.render(text, True, (255,255, 255)), (int(self.x-self.r/2), int(self.y-(self.r/4))))
     
     #Operations to be performed on key press(i.e moving the player Blob)
     def key_operation(self):
@@ -161,32 +59,111 @@ class Player:
         elif keys[pygame.K_DOWN] and self.y<height-self.r:
             self.y+=0.25
 
+    #Get the position of blob
     def getposition(self):
         return self.x,self.y,self.r
 
+    #set the position of the blob
     def setposition(self,x,y,r):
         self.x=x
         self.y=y
         self.r=r
 
-class Network():
-
+class Game():
+    #Initialising the variables
     def __init__(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect(("127.0.0.1",5001))
+        self.counter=0
+        self.player=Player((0,0,0))
+        self.anotherplayer=[Player((random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))),Player((random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))),Player((random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))),Player((random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))),Player((random.randrange(0,255),random.randrange(0,255),random.randrange(0,255)))]
+        self.network=Network()
+        self.randomlist=[]
+        self.running=True
+        self.gamerunning=True
 
-    def send(self,datatobesent):
-        msg = pickle.dumps(datatobesent)
-        self.s.send(msg)
+    #Updating the list of random points
+    def updaterandomlist(self,list1):
+        self.randomlist=list1
 
-    def recieve(self):
-        msg = self.s.recv(4096)
-        fullMsg=pickle.loads(msg)
-        return fullMsg
+    #Returns the list of random points
+    def getrandomlist(self):
+        return self.randomlist
+
+    #Redraw all the points on the plane
+    def redraw(self):
+        screen.fill(background_colour)
+        for i in range(len(self.randomlist)):
+            color=(self.randomlist[i][0],self.randomlist[i][1],self.randomlist[i][2])
+            pygame.draw.circle(screen, color, (self.randomlist[i][3],self.randomlist[i][4]), 3)
+
+    #Update the blobs of all other players
+    def updatedata(self,fullMsg):
+        self.updaterandomlist(fullMsg[0])
+        self.player.r=fullMsg[3]
+        for i in range(len(fullMsg[1])):
+            self.anotherplayer[i].setposition(fullMsg[1][i][0],fullMsg[1][i][1],fullMsg[1][i][2])
+            x1,y1,r1=self.player.getposition()
+            x2,y2,r2=fullMsg[1][i][0],fullMsg[1][i][1],fullMsg[1][i][2]
+            if ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) < ((r1 + r2) * (r1 + r2)):
+                if r1<r2:
+                    self.player.setposition(x1,y1,0)
+                    self.network.send([self.getrandomlist(),x1,y1,0])
+                    self.gamerunning=False
     
-if __name__ == "__main__":
-    import socket
-    import pickle
+    #To draw blob of other players
+    def runOtherPlayerBlob(self,fullMsg):
+         for i in range(len(fullMsg[1])):
+            self.anotherplayer[i].run()
 
+    #Update the Counter
+    def updateCounter(self,fullMsg):
+        self.counter=fullMsg[2]
+        text = str(self.counter)
+        font = pygame.font.SysFont('Consolas', 30)
+        screen.blit(font.render(text, True, (0, 0, 0)), (10, 10))
+        if self.counter==100:
+            self.gamerunning=False
+
+    #To check for radius of each player to declare the winner
+    def results(self):
+        x1,y1,r1=self.player.getposition()
+        radius_array=[0]*5
+        for i in range(5):
+            x2,y2,radius_array[i]=self.anotherplayer[i].getposition()
+        if r1>max(radius_array):
+            text="Game Over, You WON"
+        else:
+            text="Game Over, You LOSE"
+
+        font = pygame.font.SysFont('Consolas', 60)
+        screen.fill(background_colour)
+        screen.blit(font.render(text, True, (0, 0, 0)), (50, 400))
+
+    #Runs the Game
+    def run(self):
+
+        while self.running:
+            if self.gamerunning:
+                x,y,r=self.player.getposition()
+                self.network.send([self.getrandomlist(),x,y,r])
+                fullMsg=self.network.recieve()
+                self.updatedata(fullMsg)
+                self.redraw()
+                self.runOtherPlayerBlob(fullMsg)
+                self.updateCounter(fullMsg)
+                self.player.run()
+                self.player.key_operation()
+                self.player.renderText()
+                pygame.display.update()
+            else:
+                self.results()
+                pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.network.s.close()
+                    self.running = False
+                    
+
+if __name__ == "__main__":
     AgarIO=Game()
     AgarIO.run()
--- Agar.IO_Server.py
+++ Agar.IO_Server.py
@@ -2,107 +2,125 @@ import socket
 import time
 import pickle
 import _thread
-
+import random
 PORT = 5001
+class Network():
 
-#setting the constant to send the data in formatted way
-HEADER_LENGTH = 10
-
-#AF_INET -> ipv4, SOCK_STREAM -> tcp connection
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#gethostname -> refering to self on port 8080
-s.bind(("127.0.0.1",PORT))
-
-#request queue of size 5
-s.listen(5)
-
-
-#start listening
-print(f'Listening on port {PORT}')
-
-fullMsg=[]
-
-flag1=1
-flag2=1
-
-fullMsg=[]
-completedata=[[]]*5
-totalcount=0
-
-counter=0
-
-count=0
-def on_new_client(c,addr,count):
-    global counter,fullMsg,completedata,totalcount
-    while True:
+    #Intilizing the socket and connecting to server
+    def __init__(self):
         
-        if count==0:
-            msg = c.recv(4096)
-            fullMsg=pickle.loads(msg)
-
-            completedata[0]=[fullMsg[1],fullMsg[2],fullMsg[3]]
-            
-            counter=fullMsg[4]
-
-            time.sleep(0.0001)
-
-            tempdata=[fullMsg[0],completedata[1:totalcount]]
-            msg = pickle.dumps(tempdata)
-            c.send(msg)
-
-        elif count==1:
-            msg = c.recv(4096)
-            fullMsg=pickle.loads(msg)
+        HEADER_LENGTH = 10
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind(("127.0.0.1",PORT))
+        self.s.listen(5)
+        print(f'Listening on port {PORT}')
     
-            completedata[1]=[fullMsg[1],fullMsg[2],fullMsg[3]]
-            
-            time.sleep(0.0001)
-            
-            tempdata=[fullMsg[0],completedata[:1]+completedata[2:totalcount],counter]
-            msg = pickle.dumps(tempdata)
-            c.send(msg)
+    #Send randompoints,other player blob's position,counter to all other players
+    def send(self,c,datatobesent):
+        msg = pickle.dumps(datatobesent)
+        c.send(msg)
 
-        elif count==2:
-            msg = c.recv(4096)
-            fullMsg=pickle.loads(msg)
+    #Recieve randompoints and other players blob's position
+    def recieve(self,c):
+        msg = c.recv(4096)
+        fullMsg=pickle.loads(msg)
+        return fullMsg
 
-            completedata[2]=[fullMsg[1],fullMsg[2],fullMsg[3]]
-            
-            time.sleep(0.001)
-            
-            tempdata=[fullMsg[0],completedata[:2]+completedata[3:totalcount],counter]
-            msg = pickle.dumps(tempdata)
-            c.send(msg)
+class GameServer():
 
-        elif count==3:
-            msg = c.recv(4096)
-            fullMsg=pickle.loads(msg)
+    def __init__(self):
+        self.randomlist=[]
+        self.fullMsg=[]
+        self.flag1=1
+        self.flag2=1
+        self.fullMsg=[]
+        self.completedata=[[]]*5
+        self.totalcount=0
+        self.count=0
+        self.counterv=0
+        self.netwok=Network()
+               
+    #Thread to increment the count afer one sec 
+    def counter(self):
+        while True:
+            time.sleep(1)
+            self.counterv+=1
 
-            completedata[3]=[fullMsg[1],fullMsg[2],fullMsg[3]]
-            
-            time.sleep(0.0001)
-            
-            tempdata=[fullMsg[0],completedata[:3]+completedata[4:totalcount],counter]
-            msg = pickle.dumps(tempdata)
-            c.send(msg)
+    #Refresh the points on the game list
+    def refreshpoint(self):
+        if len(self.randomlist)<100:
+            self.randomlist=[]
+            for i in range(200):
+                randomele=[random.randrange(0,255),random.randrange(0,255),random.randrange(0,255),random.randrange(0,800),random.randrange(0,800)]
+                self.randomlist.append(randomele)
 
-        elif count==4:
-            msg = c.recv(4096)
-            fullMsg=pickle.loads(msg)
+    #Remove the points which lie in areas of player Blob's
+    def removepoints(self,rl,x,y,r):
+        for i in rl:
+            if ((i[3] - int(x))**2 + (i[4] - int(y))**2 )< int(r)**2:
+                r+=0.5
+                rl.remove(i)
+        return rl,r
 
-            completedata[4]=[fullMsg[1],fullMsg[2],fullMsg[3]]
-            
-            time.sleep(0.0001)
-            
-            tempdata=[fullMsg[0],completedata[:4]+completedata[5:totalcount],counter]
-            msg = pickle.dumps(tempdata)
-            c.send(msg)
-            
-while True:
-    c, addr = s.accept()
-    _thread.start_new_thread(on_new_client,(c,addr,count))
-    count+=1
-    totalcount+=1
+    #Run this thread after every new client is added to exchage object between client and the server
+    def on_new_client(self,c,addr,count):
+        while True:
 
+            if count==0:
+                self.fullMsg=self.netwok.recieve(c)
+                self.completedata[0]=[self.fullMsg[1],self.fullMsg[2],self.fullMsg[3]]
+                self.randomlist,r1=self.removepoints(self.fullMsg[0],self.fullMsg[1],self.fullMsg[2],self.fullMsg[3])
+                self.refreshpoint()
+                time.sleep(0.0001)
+                tempdata=[self.randomlist,self.completedata[1:self.totalcount],self.counterv,r1]
+                self.netwok.send(c,tempdata)
+
+            elif count==1:
+                self.fullMsg=self.netwok.recieve(c)
+                self.completedata[1]=[self.fullMsg[1],self.fullMsg[2],self.fullMsg[3]]
+                self.randomlist,r2=self.removepoints(self.fullMsg[0],self.fullMsg[1],self.fullMsg[2],self.fullMsg[3])
+                self.refreshpoint()
+                time.sleep(0.0001)
+                tempdata=[self.randomlist,self.completedata[:1]+self.completedata[2:self.totalcount],self.counterv,r2]
+                self.netwok.send(c,tempdata)
+
+            elif count==2:
+                self.fullMsg=self.netwok.recieve(c)
+                self.completedata[2]=[self.fullMsg[1],self.fullMsg[2],self.fullMsg[3]]
+                self.randomlist,r3=self.removepoints(self.fullMsg[0],self.fullMsg[1],self.fullMsg[2],self.fullMsg[3])
+                self.refreshpoint()
+                time.sleep(0.001)
+                tempdata=[self.randomlist,self.completedata[:2]+self.completedata[3:self.totalcount],self.counterv,r3]
+                self.netwok.send(c,tempdata)
+
+            elif count==3:
+                self.fullMsg=self.netwok.recieve(c)
+                self.completedata[3]=[self.fullMsg[1],self.fullMsg[2],self.fullMsg[3]]  
+                self.randomlist,r4=self.removepoints(self.fullMsg[0],self.fullMsg[1],self.fullMsg[2],self.fullMsg[3])
+                self.refreshpoint()
+                time.sleep(0.0001)
+                tempdata=[self.randomlist,self.completedata[:3]+self.completedata[4:self.totalcount],self.counterv,r4]
+                self.netwok.send(c,tempdata)
+
+            elif count==4:
+                self.fullMsg=self.netwok.recieve(c)
+                self.completedata[4]=[self.fullMsg[1],self.fullMsg[2],self.fullMsg[3]]
+                self.randomlist,r5=self.removepoints(self.fullMsg[0],self.fullMsg[1],self.fullMsg[2],self.fullMsg[3])
+                self.refreshpoint()
+                time.sleep(0.0001)
+                self.tempdata=[self.fullMsg[0],self.completedata[:4]+self.completedata[5:self.totalcount],self.counterv,r5]
+                self.netwok.send(c,tempdata)
     
+    #To run the game server
+    def run(self):
+        while True:
+            c, addr = self.netwok.s.accept()
+            if self.count==0:
+                _thread.start_new_thread(self.counter,())    
+            _thread.start_new_thread(self.on_new_client,(c,addr,self.count))
+            self.count+=1
+            self.totalcount+=1
+
+if __name__ == "__main__":
+    agarIOServer=GameServer()
+    agarIOServer.run()
